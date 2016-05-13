3ebe3c9ea77967fe0f402b3ce2d2bd210ba14caf
Trammell Hudson <hudson@trmm.net>
2016-05-12 23:04:51 -0400

    motor stepping "works" with analog speed control

--- brain/brain.ino
+++ brain/brain.ino
@@ -5,12 +5,33 @@ Furby Brain
 #define SOUND_IN 21
 #define TILT 20
 #define UPSIDE_DN 19
-#define MOTOR_FOR 18
-#define MOTOR_REV 17
+#define MOTOR_FOR 4 // PWM
+#define MOTOR_REV 5 // PWM
 #define XCAM_HOME 0
 #define GEAR_ROT 1
 #define XBACK 2
 #define TUMMY 3
+
+int desired_position = 0;
+int position;
+
+
+void motor(int dir)
+{
+	if(dir == 0)
+	{
+		digitalWrite(MOTOR_REV, LOW);
+		digitalWrite(MOTOR_FOR, LOW);
+	} else
+	if (dir > 0)
+	{
+		digitalWrite(MOTOR_REV, LOW);
+		analogWrite(MOTOR_FOR, dir);
+	} else {
+		digitalWrite(MOTOR_FOR, LOW);
+		analogWrite(MOTOR_REV, dir);
+	}
+}
 
 void setup() {
 	pinMode( MOTOR_FOR, OUTPUT );
@@ -23,29 +44,90 @@ void setup() {
 	pinMode( TUMMY, INPUT_PULLUP );
 
 	pinMode( XCAM_HOME, INPUT_PULLUP );
-	//digitalWrite( XCAM_HOME, HIGH ); // pullup
 	pinMode( XBACK, INPUT_PULLUP );
 
+	// find the home position
+	Serial.println("Finding home");
+	const int start_time = millis();
+
+	motor(+150);
+
+	while(digitalRead(XCAM_HOME))
+	{
+		if (millis() - start_time > 5000)
+			break;
+	}
+
+	motor(0);
+
+	position = 0;
+
+	delay(5000);
 }
 
 
-int position;
 int last_position_max;
 bool last_rotation;
 bool last_home_sensor;
 
+int step = 0;
+int positions[] = {
+	10,
+	20,
+	30,
+	40,
+	50,
+	60,
+	70,
+	80,
+	90,
+	100,
+	110,
+	120,
+	130,
+	50,
+	10,
+	50,
+	90,
+	130,
+	90,
+	50,
+};
+
+const int num_positions = sizeof(positions) / sizeof(*positions);
+int last_millis;
+
 void loop() 
 {
+/*
+	bool forward_pressed = (digitalRead(XBACK) == LOW);	
+	bool reverse_pressed = (digitalRead(TUMMY) == LOW);	
+
+	if (forward_pressed)
+	{
+		desired_position = 10;
+	} else
+	if (reverse_pressed)
+	{
+		desired_position = 50;
+	}
+*/
+	int now = millis();
+	if (now - last_millis > 1000)
+	{
+		last_millis = now;
+		desired_position = positions[step];
+		step = (step + 1) % num_positions;
+	}
+	
+
 	updateMotor();
 }
 
 void updateMotor()
 {
-	int sounds = analogRead(SOUND_IN); // returns int (0 to 1023)
+	//int sounds = analogRead(SOUND_IN); // returns int (0 to 1023)
 	//Serial.println(sounds - 960);
-
-	bool forward_pressed = (digitalRead(XBACK) == LOW);	
-	bool reverse_pressed = (digitalRead(TUMMY) == LOW);
 
 	bool home_sensor = digitalRead(XCAM_HOME) == LOW;
 	bool rotation_step = digitalRead(GEAR_ROT) == LOW;
@@ -70,26 +152,22 @@ void updateMotor()
 	}
 	last_home_sensor = home_sensor;
 
-	Serial.print(forward_pressed); Serial.print(' ');
-	Serial.print(reverse_pressed); Serial.print(' ');
 	Serial.print(rotation_step); Serial.print(' ');
 	Serial.print(home_sensor); Serial.print(' ');
 
-	// Don't do anything if both are pressed
-	if (!forward_pressed && !reverse_pressed) {
-		digitalWrite(MOTOR_REV, LOW);
-		digitalWrite(MOTOR_FOR, LOW);
+	if (desired_position == position) {
+		motor(0);
 	}
 
-	if (forward_pressed) {
-		digitalWrite(MOTOR_REV, LOW);
-		digitalWrite(MOTOR_FOR, HIGH);
+	int delta = desired_position - position;
+
+	if (desired_position > position) {
+		motor(+150);
 		position += rotation_delta;
 	}
 
-	if (reverse_pressed) {
-		digitalWrite(MOTOR_REV, HIGH);
-		digitalWrite(MOTOR_FOR, LOW);
+	if (desired_position < position) {
+		motor(-150);
 		position -= rotation_delta;
 	}
 
